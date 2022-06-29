6918c45bdb61edb9d88316af6db31e95e2e27c06
Kwame Obeng Yeboah <kwameobengyeboah@live.com>
2022-06-29 14:18:53 +0300

    All assignments completed

---
+++ DAY01/homework/datatypes.js
@@ -0,0 +1,15 @@
+let greetings = `Today is a good day`; //string
+console.log(typeof greetings);
+
+let isBoss = true; //boolean
+console.log(typeof isBoss);
+
+let schoolName; //undefined
+console.log(typeof schoolName);
+
+let newAge = null;
+console.log(typeof newAge);
+
+let firstName;
+let lastName;
+let maritalStatus;
